8341ce82ee5447e2eb28aae4cb7c3503e3c3068b
SanjayKumar-M <sanjaymoad786@gmail.com>
2022-11-15 23:07:02 +0530

    script

--- contracts/Kryptage.sol
+++ contracts/Kryptage.sol
@@ -1,9 +1,11 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+//SPDX-License-Identifier:MIT
+pragma solidity^0.8.7;
 
 contract Kryptage{
-    string message = "Hello This is kryptage";
-   function greet() public view returns(string memory){
-    return message;
-   }
+    string Hello;
+    function sayHello() public view returns(string memory){
+        string memory Hello1 = Hello;
+        return Hello1;
+    }
+
 }
