655c09ba4932f2b577b3d9463ef48b55b8780188
SanjayKumar-M <sanjaymoad786@gmail.com>
2022-11-16 18:54:58 +0530

    again

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,26 +1,22 @@
 const hardhat = require('hardhat')
 
 const main = async () => {
-  const Kryptage = await hardhat.ethers.getContractFactory('Kryptage')
-  const Contract = await Kryptage.deploy()
 
-  await Contract.deployed()
-  console.log("Contract deployed to ", Contract.address)
-
-  console.log("Interacting with smart contracts")
-  const Hello = await sayHello.retrieve()
-  console.log(Hello)
+    const Kryptage = await hardhat.ethers.getContractFactory('Kryptage')
+    const Contract = await Kryptage.deploy()
+    await Contract.deployed()
+    console.log(`Contract is successfully deployed to ${Contract.address}`)
 
 }
 
-(async ()=>{
-  try{
-    await main()
-    process.exit(0)
-  }
-  catch(err){
-    console.log(err)
-    process.exit(1)
-  }
-}) ()
 
+(async()=> {
+    try{
+      await main()
+      process.exit(0)
+    }
+    catch(err){
+      console.log(err)
+      process.exit(1)
+    }
+})()
