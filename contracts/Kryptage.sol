15cdaceb51f7a3d435039fbb2fbb13dcd5bfe754
SanjayKumar-M <sanjaymoad786@gmail.com>
2022-11-17 23:15:36 +0530

    update

--- contracts/Kryptage.sol
+++ contracts/Kryptage.sol
@@ -1,11 +1,20 @@
 //SPDX-License-Identifier:MIT
-pragma solidity^0.8.7;
+pragma solidity ^0.8.7;
 
-contract Kryptage{
-    string Hello;
-    function sayHello() public view returns(string memory){
-        string memory Hello1 = Hello;
-        return Hello1;
+contract Kryptage {
+    string Name = "Kryptage";
+
+    //declaring class of File
+    struct File {
+        string Name;
+        string Type;
+        string Description;
+        string fileHash;
+        uint Id;
+        uint Timestamp;
+        address payable owner;
     }
-
+   //mapping the file id to the class File (Nested mapping)
+   //declaring a public mappung t
+    mapping (uint => File) public _file;
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,22 +1,20 @@
-const hardhat = require('hardhat')
+const hardhat = require("hardhat")
 
-const main = async () => {
-
-    const Kryptage = await hardhat.ethers.getContractFactory('Kryptage')
-    const Contract = await Kryptage.deploy()
-    await Contract.deployed()
-    console.log(`Contract is successfully deployed to ${Contract.address}`)
+const main = async () =>{
+  const Kryptage = await hardhat.ethers.getContractFactory('Kryptage');
+  const Contract = await Kryptage.deploy()
+  await Contract.deployed(); 
+  console.log(`Contract is deployed successfully to ${Contract.address}`)
 
 }
 
-
-(async()=> {
-    try{
-      await main()
-      process.exit(0)
-    }
-    catch(err){
-      console.log(err)
-      process.exit(1)
-    }
+(async() =>{
+  try{
+    await main()
+    process.exit(0)
+  }
+  catch(err){
+    await err
+    process.exit(1)
+  }
 })()
