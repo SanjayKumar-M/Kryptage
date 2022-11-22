f8d535a091c71d87488038dba1b6143d6018c07e
SanjayKumar-M <sanjaymoad786@gmail.com>
2022-11-22 23:26:37 +0530

    again

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,7 +1,64 @@
-import React from 'react'
+import { React, useState } from 'react'
 import '../Navbar.css'
 import { ethers } from 'ethers'
+import { ConnectWallet } from '@thirdweb-dev/react';
+
 function Navbar() {
+  const [currentAccount, setCurrentAccount] = useState('');
+
+  const [network, setNetwork] = useState('');
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask -> https://metamask.io/");
+        return;
+      }
+      const account = await ethereum.request({ method: "eth_requestAccounts" });
+
+
+      console.log("Connected", account[0]);
+      setCurrentAccount(account[0]);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+
+  const checkIfWalletIsConnected = async () => {
+
+
+    const { ethereum } = window;
+    if (!ethereum) {
+      alert("Please install or activate Metamask wallet");
+      return;
+    } else {
+      console.log("Haha", ethereum);
+
+    }
+    const account = await ethereum.request({ method: "eth_Accounts" });
+    if (account.length !== 0) {
+      const accounts = account[0];
+      console.log('Found an authorized account:', accounts);
+      setCurrentAccount(accounts);
+    } else {
+      console.log('No authorized account found');
+    }
+
+    const chainId = await ethereum.request({ method: 'eth_chainId' });
+    setNetwork(networks[chainId]);
+
+    ethereum.on('chainChanged', handleChainChanged);
+
+
+    function handleChainChanged(_chainId) {
+      window.location.reload();
+    }
+
+
+  }
+
 
   return (
     <nav>
@@ -11,7 +68,7 @@ function Navbar() {
       <div className='right'>
         <ul>
           <li> <a href='/'>Recent</a></li>
-          <li></li>
+          <li><button onClick={connectWallet}>CONNECT WALLET</button></li>
         </ul>
 
 
