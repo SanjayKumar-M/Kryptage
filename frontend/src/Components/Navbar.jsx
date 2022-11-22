823c123639ed87f8434b7d44598f6113f1d3d6ee
SanjayKumar-M <sanjaymoad786@gmail.com>
2022-11-22 19:47:50 +0530

    update

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,19 +1,9 @@
 import React from 'react'
 import '../Navbar.css'
-import "https://cdnjs.cloudflare.com/ajax/libs/web3/1.7.4-rc.1/web3.min.js"
 
 function Navbar() {
 
-  const Connect = async() =>{
-    if(window.ethereum){
-      await window.ethereum.request({method: "eth_requestAccounts" })
-      window.web3(window.ethereum);
   
- } else {
-  console.log("No wallet");
- }
-
-    }
   
   return (
     <nav>
@@ -23,7 +13,7 @@ function Navbar() {
       <div className='right'>
         <ul>
           <li> <a href='/'>Recent</a></li>
-          <li><button onClick={Connect} >CONNECT WALLET</button> </li>
+          <li><ConnectWallet accentColor="#f213a4" colorMode="dark" /></li>
         </ul>
 
 
