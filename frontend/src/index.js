1f9fecd52480e24be152780f8ec421cac61df436
SanjayKumar-M <sanjaymoad786@gmail.com>
2022-11-22 13:14:45 +0530

    again

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-import Kryptage from './App';
+import Kryptage from './Components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
