521780b2f713100385f3624a398f6e1ec5676606
SanjayKumar-M <sanjaymoad786@gmail.com>
2022-11-22 13:11:01 +0530

    align

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,16 @@ import React from 'react'
 import './App.css'
 const Kryptage = () => {
 
-  
+
   return (
-   <div className='container'>
+    <div className='container'>
       <nav>
         <div className='logo'>Kryptage</div>
       </nav>
       <div className='side'>
         <button>CONNECT WALLET</button>
       </div>
-   </div>
+    </div>
   )
 }
 
