b057678093dd942d6e701fcffe2144a993c9f332
SanjayKumar-M <sanjaymoad786@gmail.com>
2022-11-26 22:57:23 +0530

    forms

--- frontend/src/Components/Main.js
+++ frontend/src/Components/Main.js
@@ -26,27 +26,6 @@ const Main = () => {
         </div>
       </nav>
       <div className='main'>
-        <Box
-          component="form"
-          sx={{
-            '& .MuiTextField-root': { m: 1, width: '25ch' },
-          }}
-          noValidate
-          autoComplete="off"
-        />
-        <TextField
-          id="outlined-multiline-flexible"
-          label="Multiline"
-          multiline
-          maxRows={4}
-          value={value}
-          onChange={handleChange}
-          variant="filled"
-        />
-        <Button variant="contained" component="label">
-          Upload
-          <input hidden accept="/*" multiple type="file" />
-        </Button>
       </div>
     </div>
   )
