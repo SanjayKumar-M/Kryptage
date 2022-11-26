6e3ba46720f6e3dddb67d8a3b017543e12ddfdc2
SanjayKumar-M <sanjaymoad786@gmail.com>
2022-11-26 22:23:26 +0530

    deleted main

--- frontend/src/Components/Main.js
+++ frontend/src/Components/Main.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import '../Styles/Navbar.css'
-import '../Styles/Main.css'
 
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 
@@ -8,28 +7,21 @@ const Main = () => {
 
   return (
 
-    <>
+    <div >
       <nav>
         <div className='left'>
           <img src='https://raw.githubusercontent.com/SanjayKumar-M/Kryptage/main/frontend/src/Utils/Kryptage.png' />
         </div>
         <div className='right'>
           <ul>
-
             <li><ConnectButton /></li>
           </ul>
-
-
         </div>
-
       </nav>
-      <div class="container">
-        <div class="name">Register Now</div>
-        <form action="#">
-
-        </form>
+      <div className='main'>
+        Hello
       </div>
-    </>
+    </div>
   )
 }
 
