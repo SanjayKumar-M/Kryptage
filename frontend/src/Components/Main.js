af689100cfb502b4c9cf6a4f6707cb96b9250d8f
SanjayKumar-M <sanjaymoad786@gmail.com>
2022-11-26 22:53:27 +0530

    mui buttons

--- frontend/src/Components/Main.js
+++ frontend/src/Components/Main.js
@@ -3,8 +3,14 @@ import '../Styles/Navbar.css'
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import { ConnectButton } from "@rainbow-me/rainbowkit";
-
+import Button from '@mui/material/Button';
 const Main = () => {
+  const [value, setValue] = React.useState('Controlled');
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
 
   return (
 
@@ -20,7 +26,27 @@ const Main = () => {
         </div>
       </nav>
       <div className='main'>
-        Hello
+        <Box
+          component="form"
+          sx={{
+            '& .MuiTextField-root': { m: 1, width: '25ch' },
+          }}
+          noValidate
+          autoComplete="off"
+        />
+        <TextField
+          id="outlined-multiline-flexible"
+          label="Multiline"
+          multiline
+          maxRows={4}
+          value={value}
+          onChange={handleChange}
+          variant="filled"
+        />
+        <Button variant="contained" component="label">
+          Upload
+          <input hidden accept="/*" multiple type="file" />
+        </Button>
       </div>
     </div>
   )
