c605a8f223f0b73fe9614b32ba0278c6a1212889
SanjayKumar-M <sanjaymoad786@gmail.com>
2022-11-28 22:41:18 +0530

    modify

--- frontend/src/Components/App.js
+++ frontend/src/Components/App.js
@@ -1,6 +1,7 @@
 import '../Styles/App.css'
-import Main  from './Main'
-
+import Main from './Main'
+import Web3 from 'web3'
+// import Kryptage from '../Utils/kryptageABI.json'
 import '@rainbow-me/rainbowkit/styles.css';
 import {
   getDefaultWallets,
@@ -16,7 +17,7 @@ import { alchemyProvider } from 'wagmi/providers/alchemy';
 import { publicProvider } from 'wagmi/providers/public';
 
 const { chains, provider } = configureChains(
-  [chain.mainnet, chain.polygon, chain.hardhat,chain.goerli, chain.polygonMumbai,],
+  [chain.mainnet, chain.polygon, chain.hardhat, chain.goerli, chain.polygonMumbai,],
   [
     alchemyProvider({ apiKey: "7LGvqMTpRaoj6JIzhVdCYZW-GWmnOihO" }),
     publicProvider()
@@ -33,20 +34,19 @@ const wagmiClient = createClient({
   connectors,
   provider
 })
-const Kryptage = () => {
-
-
+const App = () => {
+  
   return (
     <div className='container'>
       <WagmiConfig client={wagmiClient}>
         <RainbowKitProvider chains={chains}>
 
           <Main />
-          
+
         </RainbowKitProvider>
       </WagmiConfig>
 
     </div>
   )
 }
-export default Kryptage
+export default App
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import '../src/Styles/index.css';
 
-import Kryptage from './Components/App';
+import App from './Components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Kryptage />
+    <App />
 );
 
